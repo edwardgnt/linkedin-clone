26f3bd7a90e83663e59f04ade22ea85926462780
Edward Güereque <edwardgnt@gmail.com>
2022-06-17 16:53:42 -0700

    Added sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,7 +6,7 @@ const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className="sidebar__top">
-            <img src="" alt="" />
+            <img src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80" alt="" />
             <Avatar className='sidebar__avatar' />
             <h2>Edward Güereque</h2>
             <h4>edwardgnt@gmail.com</h4>
